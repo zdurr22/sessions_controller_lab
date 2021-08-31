f389ba1d9f3003f68824fffdf03c352b6b3c8e09
zdurr22 <zdurr22@gmail.com>
2021-08-31 12:13:45 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,7 @@ class SessionsController < ApplicationController
     end
   
     def create
-      return redirect_to(controller: 'sessions',
-                         action: 'new') if !params[:name] || params[:name].empty?
+      return redirect_to(controller: 'sessions', action: 'new') if !params[:name] || params[:name].empty?
       session[:name] = params[:name]
       redirect_to controller: 'application', action: 'hello'
     end
